8f5fe3219af481d1f2094417850c4b0c33a04131
Youke XIANG <opp100@qq.com>
2018-09-01 18:05:28 +1000

    Chore: added typescript support

---
+++ server/dist/server/index.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello world!\n');
+});
+//initialize a simple http server
+const server = http.createServer(app);
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+wss.on('connection', (ws) => {
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message) => {
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+    //send immediatly a feedback to the incoming connection
+    ws.send('Hi there, I am a WebSocket server');
+});
+//start our server
+server.listen(process.env.PORT || 8080, () => {
+    console.log(`Server started on port ${server.address()} :)`);
+});
+//# sourceMappingURL=index.js.map
